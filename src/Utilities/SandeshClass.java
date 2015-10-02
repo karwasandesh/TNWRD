55940d1ed98a6b9fd1f63f6ac6a057cb1285a0a3
karwasandesh <karwa.sandesh@gmail.com>
2015-10-02 16:19:29 +0530

    SandeshClass Added to Utility

---
+++ src/Utilities/SandeshClass.java
@@ -0,0 +1,15 @@
+package Utilities;
+
+public class SandeshClass {
+/*
+ * 
+ * asdfnjkasddnfjkansdfjklbasdjklbfasjldkdbfjkasbdfjkbasdkjbfasdjklbf
+ * asdfjkhasddjkfnajksddnfjkasdnf
+ * sadfnasddbfnjkasjdfuilhasduifi
+ * asdfbasdbdfjklabsduifbasduf
+ * asfdaksdbfuiabsdkfjnasdjklfhuiasdbfjkasdnfiuahsduifnasdjklnfuis
+ * 
+ * 
+ * 
+ */
+}
